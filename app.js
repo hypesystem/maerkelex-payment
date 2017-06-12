0dc38a5ec579f3103e7df0d8d76a65ffb3a83af9
Niels Abildgaard <niels.abildgaard@gmail.com>
2017-06-12 20:54:11 +0200

    Make cookie session secret something configured by env

--- app.js
+++ app.js
@@ -8,22 +8,18 @@ const path = require("path");
 const errorView = fs.readFileSync(path.join(__dirname, "_errors/default.html")).toString();
 const badRequestErrorView = fs.readFileSync(path.join(__dirname, "_errors/400.html")).toString();
 const mustache = require("mustache");
-const cookieSession = require("cookie-session");
 
-module.exports = (maerkelex, paymentGateway, db, mailer) => {
+module.exports = (maerkelex, paymentGateway, db, mailer, cookieSession) => {
     let app = express();
 
-    let purchases = Purchases(maerkelex, paymentGateway, db, mailer);
-    
+    let purchases = Purchases(maerkelex, paymentGateway, db, mailer, cookieSession);
+
     app.use(bodyParser.urlencoded({
         extended: false
     }));
 
-    app.use(cookieSession({
-        name: "maerkelex-payment-session-cookie",
-        secret: "abb304c8-4b8e-4668-a744-451770259de0"
-    }));
-    
+    app.use(cookieSession);
+
     app.use((req, res, next) => {
         res.fail = (statusCode, message, failedAction) => {
             if(typeof message !== "string") {
--- start.js
+++ start.js
@@ -5,6 +5,7 @@ var pkg = require("./package.json");
 var maerkelexApi = require("./maerkelex/api.js");
 var MailgunMustacheMailer = require("mailgun-mustache-mailer");
 var Pool = require("pg-pool");
+const cookieSession = require("cookie-session");
 
 config.braintree.environment = braintree.Environment[config.braintree.environment];
 
@@ -12,8 +13,12 @@ var maerkelex = maerkelexApi(config.maerkelex);
 var paymentGateway = braintree.connect(config.braintree);
 var db = new Pool(config.postgres);
 var mailer = new MailgunMustacheMailer(config.mailgun);
+let cookieSessionInstance = cookieSession({
+    name: "maerkelex-payment-session-cookie",
+    secret: "abb304c8-4b8e-4668-a744-451770259de0"
+});
 
-var app = maerkelexPaymentApp(maerkelex, paymentGateway, db, mailer);
+var app = maerkelexPaymentApp(maerkelex, paymentGateway, db, mailer, cookieSessionInstance);
 
 app.listen(3000);
 
