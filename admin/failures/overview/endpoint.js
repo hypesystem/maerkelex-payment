3032a0415f8e7b489dfff666776bdb05fd9f1871
Niels Roesen Abildgaard <niels.abildgaard@gmail.com>
2020-05-25 11:59:59 +0200

    Add admin page for viewing failure data (v quick rough)

---
+++ admin/failures/overview/endpoint.js
@@ -0,0 +1,50 @@
+const fs = require("fs");
+const path = require("path");
+const view = fs.readFileSync(path.join(__dirname, "view.html")).toString();
+const mustache = require("mustache");
+
+module.exports = (purchases) => (req, res) => {
+    purchases.list((error, orders) => {
+        if(error) {
+            console.error("Failed to list purchase", error);
+            return res.fail(500);
+        }
+
+        let failedOrders = orders
+            .filter(x => x.data.errors && x.data.errors.length)
+            .sort((a, b) => {
+                const aLatest = a.data.errors[a.data.errors.length - 1];
+                const bLatest = b.data.errors[b.data.errors.length - 1];
+                if(aLatest < bLatest) {
+                    return -1;
+                }
+                if(aLatest > bLatest) {
+                    return 1;
+                }
+                return 0;
+            })
+            .map((o) => {
+                return {
+                    id: o.id,
+                    latestErrorAt: prettifyDate(o.data.errors[o.data.errors.length - 1].at),
+                    errors: o.data.errors.map((error) => {
+                        return {
+                            at: prettifyDate(error.at),
+                            data: error.data && JSON.stringify(error.data, null, 4),
+                            description: error.description,
+                        };
+                    }),
+                    description: `${o.data.viewModel.customerInfo.name}<br>${o.data.viewModel.orderLines.map((l) => l.description).join("; ")}<br>${o.data.total} DKK`,
+                    viewModel: JSON.stringify(o.data.viewModel, null, 4),
+                };
+            });
+
+        res.send(mustache.render(view, { failedOrders }));
+    });
+};
+
+function prettifyDate(date) {
+    if(!date) return "";
+    let parts = date.substring(0, 10).split("-");
+    return parts[2] + "/" + parts[1] + " " + parts[0];
+}
