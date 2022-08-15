ae108689675f1b5488ba70f69f4d7ad1c5c1f750
Niels Roesen Abildgaard <niels.abildgaard@gmail.com>
2022-08-15 15:56:52 +0200

    Rewrite maerkelex/api.js to use axios over request

--- maerkelex/api.js
+++ maerkelex/api.js
@@ -1,52 +1,42 @@
-var request = require("request");
+const axios = require("axios");
 
 // Bind context to function using `b`
 const b = (fn, ...args) => fn.bind(null, ...args);
+const cbify = (promise, callback) => promise.then((data) => callback(null, data)).catch(callback);
 
 function MaerkelexApi(config) {
     return {
-        get: b(get, config.baseUrl),
-        getData: b(getData, config.baseUrl),
+        get: cbify(b(get, config.baseUrl)),
+        getData: cbify(b(getData, config.baseUrl)),
+        getAsync: b(get, config.baseUrl),
+        getDataAsync: b(getData, config.baseUrl),
     };
 }
 
-function get(baseUrl, badgeIds, callback) {
-    getData(baseUrl, (error, data) => {
-        if(error) {
-            return callback(error);
-        }
-        var badges = data.m.filter(m => badgeIds.includes(m.id));
-        if(!badges.length || badges.length != badgeIds.length) {
-            return callback({
-                type: "NotFound",
-                trace: new Error("Some requested badge IDs were not found."),
-                badgeIds: badgeIds.filter((id) => !data.m.some(m => m.id == id)),
-            });
-        }
-        callback(null, badges, {
+async function get(baseUrl, badgeIds) {
+    const data = await getData(baseUrl);
+
+    var badges = data.m.filter(m => badgeIds.includes(m.id));
+    if(!badges.length || badges.length != badgeIds.length) {
+        throw {
+            type: "NotFound",
+            trace: new Error("Some requested badge IDs were not found."),
+            badgeIds: badgeIds.filter((id) => !data.m.some(m => m.id == id)),
+        };
+    }
+
+    return {
+        badges,
+        shipping: {
             domestic: data.defaultShippingPrice,
             international: data.internationalShippingPrice,
-        });
-    });
+        }
+    };
 }
 
-function getData(baseUrl, callback) {
-    request.get(baseUrl + "/m.json", (error, httpResponse, body) => {
-        if(error) {
-            return callback(error);
-        }
-        if(httpResponse.statusCode != 200) {
-            return callback(new Error("Failed to get mærkelex data (non-200 response): " + httpResponse.statusCode));
-        }
-        var json;
-        try {
-            json = JSON.parse(body);
-        }
-        catch(e) {
-            return callback(new Error("Failed to parse json response: " + e));
-        }
-        callback(null, json);
-    });
+async function getData(baseUrl) {
+    const response = axios.get(baseUrl + "/m.json");
+    return response.data;
 }
 
 module.exports = MaerkelexApi;
