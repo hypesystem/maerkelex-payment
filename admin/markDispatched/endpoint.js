3b251e23fe908a98a6ed5529bd4205b656ac1d9f
Lucas Hayes <luca4177@gmail.com>
2023-09-27 13:41:00 +0200

    On error use the error page

--- admin/markDispatched/endpoint.js
+++ admin/markDispatched/endpoint.js
@@ -2,14 +2,14 @@ module.exports = (purchases) => (req, res) => {
     const options = req.query;
     purchases.sendReceipt(req.params.id, (error) => {
         if(error && error.type == "PurchaseNotFound") {
-            return res.send(400, "Ordren findes ikke.");
+            return res.fail(400, "Ordren findes ikke.");
         }
         if(error && error.type == "CanNotDispatchPurchase") {
-            return res.send(400, "Ordren kan ikke markeres som afsendt (enten allerede afsendt eller endnu ikke betalt).");
+            return res.fail(400, "Ordren kan ikke markeres som afsendt (enten allerede afsendt eller endnu ikke betalt).");
         }
         if(error) {
             console.error("Failed to send receipt for purchase", error);
-            return res.fail(500);
+            return res.fail(500, "Kunne ikke sende en kvitering for ordren");
         }
         if(options && options.redirectToAdmin == "false") {
             return res.send(200, "Ordren blev markeret som afsendt");
