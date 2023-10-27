71f1a54816e80dfd0eb92a2ba6eba00d9cb1e0b7
Lucas Hayes <luca4177@gmail.com>
2023-10-27 13:39:57 +0200

    refactoring for readability

--- admin/failures/overview/endpoint.js
+++ admin/failures/overview/endpoint.js
@@ -26,10 +26,9 @@ module.exports = (purchases) => (req, res) => {
             })
             .map((o) => {
                 const latestError = getLastElement(o.data.errors);
-                const latestErrorAt = latestError ? prettifyDate(latestError.at) : prettifyDate(o.data.viewModel.date);
                 return {
                     id: o.id,
-                    latestErrorAt,
+                    latestErrorAt: prettifyDate(getLatestTimestamp(o)),
                     errors: !latestError ? [] : o.data.errors.reverse().map((error) => {
                         return {
                             at: prettifyDate(error.at),
@@ -47,10 +46,11 @@ module.exports = (purchases) => (req, res) => {
 };
 
 function getLatestTimestamp(order){
-    if(!order.data.errors || !order.data.errors.length){
+    let latestError = getLastElement(order.data.errors);
+    if(!latestError){
         return order.data.viewModel.date;
     }
-    return order.data.errors[order.data.errors.length - 1].at;
+    return latestError.at;
 }
 
 function getLastElement(list) {
