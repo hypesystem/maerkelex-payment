784c30a3ae42c3f065e23cc40c7803c060c44665
Lucas Hayes <luca4177@gmail.com>
2024-06-07 16:12:52 +0200

    fill out ipAddress with actual ipAddress

--- purchase/startPurchaseEndpoint.js
+++ purchase/startPurchaseEndpoint.js
@@ -26,7 +26,7 @@ module.exports = (purchases) => (req, res) => {
         badges = [ { id: badge, count } ];
     }
 
-    parseCustomerInfo(req.body, (error, customerInfo) => {
+    parseCustomerInfo(req.body, req.ip, (error, customerInfo) => {
         if(error) {
             console.error("Failed to parse customer info", error, req.body);
             return res.fail(400, "Forespørgslen indeholdt ikke den påkrævede kundeinformation: " + error.message);
@@ -50,8 +50,10 @@ module.exports = (purchases) => (req, res) => {
     });
 };
 
-function parseCustomerInfo(body, callback) {
+function parseCustomerInfo(body, ipAddress, callback) {    
     var customer = {};
+
+    customer.ipAddress = ipAddress;
 
     var email = body["email"];
     if(!email || !/.*@.*/.test(email)) {
